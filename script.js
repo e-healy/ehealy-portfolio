6a64a691f8098c0d18b40d76fa2f116b28943b08
Ellie H <elliehealy20@outlook.com>
2024-03-05 11:01:16 +0000

    Add contact title and clock

--- script.js
+++ script.js
@@ -48,3 +48,14 @@ overlay.addEventListener('click', () => {
         closeModal(modal)
     });
 });
+
+/* Clock */
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+
+setInterval(() =>{
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+}, 1000);
