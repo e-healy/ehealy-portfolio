4fa5aaad02ea45b3f29583b2e0cf6f31731e0fcd
Ellie H <elliehealy20@outlook.com>
2024-03-09 20:33:04 +0000

    Add responsive design for smaller devices. Add project card

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ navEl.addEventListener("click", () => {
     hamburgerEl.classList.remove("hamburgerOpen")
 });
 
+//Modal buttons
+
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
